baaa7c8e771cc81409651270bd8881139f51f409
Muhammet ÇETİN <muhammedcetin94@gmail.com>
2022-06-23 11:10:50 +0300

    Repostory Pattern devam

--- Northwind.DAL/Abstract/IRepositoryBase.cs
+++ Northwind.DAL/Abstract/IRepositoryBase.cs
@@ -7,8 +7,10 @@ namespace Northwind.DAL.Abstract
         int Add(T input);
         int Update(T input);
         int Delete(T input);
+
+        T Find(int Id);
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
-        List<T> GetInclude(Expression<Func<T, bool>> filter = null,
-                           params Expression<Func<T, object>>[] include);
+        IQueryable<T> GetInclude(Expression<Func<T, bool>> filter = null,
+                            params Expression<Func<T, object>>[] include);
     }
 }
---
+++ Northwind.DAL/Concrete/RepositoryBase.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Northwind.DAL.Abstract;
+using Northwind.Entities;
+using System.Linq.Expressions;
+
+namespace Northwind.DAL.Concrete
+{
+    public class RepositoryBase<T> : IRepositoryBase<T> where T : class, new()
+    {
+        NorthwindContext db;
+        public RepositoryBase()
+        {
+            db = new NorthwindContext();
+        }
+        public int Add(T input)
+        {
+            db.Set<T>().Add(input);
+            return db.SaveChanges();
+        }
+
+        public int Update(T input)
+        {
+            db.Set<T>().Update(input);
+            return db.SaveChanges();
+        }
+        public int Delete(T input)
+        {
+            db.Set<T>().Remove(input);
+            return db.SaveChanges();
+        }
+
+        public T Find(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetAll(Expression<Func<T, bool>> filter = null)
+        {
+            if (filter == null)
+            {
+                return db.Set<T>().ToList();
+            }
+            else
+            {
+                return db.Set<T>().Where(filter).ToList();
+            }
+        }
+
+        public IQueryable<T> GetInclude(Expression<Func<T, bool>> filter = null, params Expression<Func<T, object>>[] include)
+        {
+            var query = db.Set<T>().Where(filter);
+            return include.Aggregate(query, (current, includeProperty) => current.Include(includeProperty));
+        }
+
+
+    }
+}
